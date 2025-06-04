872129c1cc89a892ffbd467650244b205e81406a
jasleensand <jasleensandhu5283@gmail.com>
2025-06-04 17:30:24 +1000

    footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,5 @@
 
 // sliding navigation menu for mobile devices
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.getElementById("menu-toggle");
-  const mobileNav = document.getElementById("mobile-nav");
-
-  menuToggle.addEventListener("click", () => {
-    mobileNav.classList.toggle("active");
-  });
-});
 
 //navigation bar color change on scroll
 window.addEventListener('scroll', () => {
